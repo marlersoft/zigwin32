0b580fcbfbd9535d3ebfd5bdeb273071646280ec
Trevor-Strong <76170333+Trevor-Strong@users.noreply.github.com>
2024-01-27 09:56:26 -0600

    Add test step and update 'source_file' to 'root_source_file'

--- build.zig
+++ build.zig
@@ -1,7 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
     _ = b.addModule("zigwin32", .{
-        .source_file = .{ .path = "win32.zig" },
+        .root_source_file = .{ .path = "win32.zig" },
+        .optimize = optimize,
+        .target = target,
     });
+
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "win32.zig" },
+        .optimize = optimize,
+        .target = target,
+        .link_libc = true,
+    });
+
+    const run_tests = b.addRunArtifact(tests);
+
+    const test_step = b.step("test", "Run tests");
+
+    test_step.dependOn(&run_tests.step);
 }
