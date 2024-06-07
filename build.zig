5c9f5e6b330b493298d42476310a6f192f375602
Jonathan Marler <johnnymarler@gmail.com>
2024-06-07 08:55:02 -0600

    use std.Build.path for newer zig versions

--- build.zig
+++ build.zig
@@ -1,16 +1,8 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-const is_zig_0_11 = std.mem.eql(u8, builtin.zig_version_string, "0.11.0");
-
 pub fn build(b: *std.Build) void {
-    if (is_zig_0_11) {
-        _ = b.addModule("zigwin32", .{
-            .source_file = .{ .path = "win32.zig" },
-        });
-    } else {
-        _ = b.addModule("zigwin32", .{
-            .root_source_file = .{ .path = "win32.zig" },
-        });
-    }
+    _ = b.addModule("zigwin32", .{
+        .root_source_file = b.path("win32.zig"),
+    });
 }
