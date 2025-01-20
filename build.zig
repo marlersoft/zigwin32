df3b213e659fa70775665337d703b078c854c78a
Jonathan Marler <johnnymarler@gmail.com>
2025-01-20 14:44:00 -0700

    rename build.zig module from zigwin32 to win32

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const builtin = @import("builtin");
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("zigwin32", .{
+    _ = b.addModule("win32", .{
         .root_source_file = b.path("win32.zig"),
     });
 }
